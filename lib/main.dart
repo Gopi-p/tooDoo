27db590a1f3c6eb1b59da24936df1c9a73951724
Gopinath <gopinath.p@tandemloop.in>
2022-04-02 13:23:57 +0530

    Integrated riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const TooDoo());
+  runApp(const ProviderScope(child: TooDoo()));
 }
 
 class TooDoo extends StatelessWidget {

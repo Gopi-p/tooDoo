5daf835d51af583afaf5e1dd05e21f435a19c353
Gopinath <gopinath.p@tandemloop.in>
2022-05-03 20:56:32 +0530

    Added textsyles in theme

--- lib/shared.module/theme.dart
+++ lib/shared.module/theme.dart
@@ -10,4 +10,64 @@ class AppTheme {
   static const tertiaryDarkColor = Color(0xff474747);
   static const tertiaryLightColor = Color(0xff8d8d8d);
   static const backgroundColor = Color(0xfff9faff);
+
+  static const TextStyle heading1 = TextStyle(
+    color: Color(0xff27466c),
+    fontSize: 32,
+    fontWeight: FontWeight.w800,
+  );
+  static const TextStyle heading2 = TextStyle(
+    color: Color(0xff474747),
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle subheading1 = TextStyle(
+    color: Color(0xff474747),
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle subheading2 = TextStyle(
+    color: Color(0xff474747),
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle buttonStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.36,
+  );
+  static const TextStyle body1 = TextStyle(
+    // color: Color(0xff8d8d8d),
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.28,
+  );
+  static const TextStyle body2 = TextStyle(
+    color: Color(0xff474747),
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle label1 = TextStyle(
+    color: Color(0xffffc046),
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.24,
+  );
+  static const TextStyle label2 = TextStyle(
+    color: Color(0xff8d8d8d),
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle lead1 = TextStyle(
+    color: Color(0xff8d8d8d),
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0.20,
+  );
+  static const TextStyle lead2 = TextStyle(
+    color: Color(0xff8d8d8d),
+    fontSize: 8,
+    letterSpacing: 0.16,
+  );
 }

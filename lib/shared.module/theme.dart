aa2f79ea36ad46ed2f14719ea8391dbd099c2303
Gopinath <gopinath.p@tandemloop.in>
2022-05-03 20:38:30 +0530

    Created Theme & added app colors

---
+++ lib/shared.module/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const black = Color(0xff000000);
+  static const white = Color(0xffffffff);
+  static const transparent = Color(0x00000000);
+
+  static const primaryColor = Color(0xff27466c);
+  static const secondaryColor = Color(0xff076bff);
+  static const tertiaryDarkColor = Color(0xff474747);
+  static const tertiaryLightColor = Color(0xff8d8d8d);
+  static const backgroundColor = Color(0xfff9faff);
+}
